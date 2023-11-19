d1ea358a23f6328de884298e859adf55b83dea65
fadillahmuhammad <fadillahmuhammad1102@gmail.com>
2023-11-19 16:19:57 +0700

    Update Folder Project

--- app/src/main/java/com/dicoding/kulinerku/MainActivity.kt
+++ app/src/main/java/com/dicoding/kulinerku/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.dicoding.kulinerku.ui.screen.splash.SplashScreen
 import com.dicoding.kulinerku.ui.theme.KulinerkuTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KulinerkuTheme {
-        Greeting("Android")
     }
 }
