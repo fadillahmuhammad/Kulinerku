e63aa28285d7d9d6a5d25a0bdb5d71657008a66f
fadillahmuhammad <fadillahmuhammad1102@gmail.com>
2023-11-19 11:18:14 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/kulinerku/ui/theme/Type.kt
@@ -0,0 +1,39 @@
+package com.dicoding.kulinerku.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.dicoding.kulinerku.R
+
+val fontFamily = FontFamily(
+    Font(R.font.poppins_regular)
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_bold)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_semi_bold)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
